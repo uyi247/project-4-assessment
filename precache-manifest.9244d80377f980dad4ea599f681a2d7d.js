86ff6de4f2d0aa1acc625857bff24b17b1af748d
Osa Obas <mobas0681@gmail.com>
2020-10-02 17:24:13 -0400

    Updates

---
+++ precache-manifest.9244d80377f980dad4ea599f681a2d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6089f1250c4e048089ec89e18bf1959",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "11acd38e495518e548e6",
+    "url": "/project-4-assessment/static/css/main.08295ecc.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11acd38e495518e548e6",
+    "url": "/project-4-assessment/static/js/main.aac5b014.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
